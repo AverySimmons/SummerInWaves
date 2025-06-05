73b915eda4c74499200f9c4b1a580f59e1e79cd0
Novali91 <joeyd@firetruk.ca>
2025-06-04 17:48:59 -0700

    Started Disc class

---
+++ 02_Source/02_Combat/Discs/disc.gd
@@ -0,0 +1,25 @@
+class_name Disc
+extends Area2D
+
+const MOMENTUM_TRANSFERED = .6
+const FRICTION = 100
+const GRAVITY = 9.8
+
+var velocity: float = 400
+var is_enemy: bool
+var sprite
+var direction: Vector2 = Vector2(1, 0)
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	position += velocity * direction.normalized() * delta
+	
+	velocity = move_toward(velocity, 0, FRICTION*delta)
+	
+	pass
+
+
+func is_moving() -> bool:
+	return velocity != 0
