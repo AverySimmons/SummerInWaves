a5fcf444b041debc28ffade50bdbe2a9e9fe6b31
Sundered7 <brian@froods.ca>
2025-06-05 14:17:23 -0700

    title screen

--- 02_Source/00_TopManagers/Managers/scene_manager.gd
+++ 02_Source/00_TopManagers/Managers/scene_manager.gd
@@ -1,1 +1,14 @@
 extends Node2D
+
+var game_manager_scene: PackedScene = preload("res://02_Source/00_TopManagers/Managers/game_manager.tscn")
+#start with title screen as a child. when play button is pressed, emit a signal
+#scene manager will switch to game manager. it adds in game manager and deletes the title
+func _ready() -> void:
+	SignalBus.start_game.connect(start_game)
+
+func start_game() -> void:
+	var new_game_manager = game_manager_scene.instantiate()
+	
+	add_child(new_game_manager)
+	
+	$TitleScreen.queue_free()
--- 02_Source/00_TopManagers/Singletons/signal_bus.gd
+++ 02_Source/00_TopManagers/Singletons/signal_bus.gd
@@ -3,3 +3,5 @@ extends Node
 signal create_disc(pos, vel, dir, is_enemy)
 
 signal switch_game(win: bool)
+
+signal start_game()
