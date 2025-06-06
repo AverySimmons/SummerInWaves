18deab09b74623f723b726a0495463e906fd7c74
Sundered7 <brian@froods.ca>
2025-06-06 12:57:34 -0700

    Dialogue pause in combat

--- 02_Source/00_TopManagers/Managers/scene_manager.gd
+++ 02_Source/00_TopManagers/Managers/scene_manager.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var game_manager_scene: PackedScene = preload("res://02_Source/00_TopManagers/Managers/game_manager.tscn")
 var settings_scene: PackedScene = preload("res://02_Source/00_TopManagers/SettingsMenu/settings.tscn")
+var in_dialogue_pause: bool = false
+var currently_paused: bool = false
 
 #start with title screen as a child. when play button is pressed, emit a signal
 #scene manager will switch to game manager. it adds in game manager and deletes the title
@@ -9,6 +11,7 @@ func _ready() -> void:
 	load("res://03_DialogicAssets/Styles/primary_style.tres").prepare()
 	Dialogic.preload_timeline("res://03_DialogicAssets/Timelines/empty_timeline.dtl")
 	SignalBus.start_game.connect(start_game)
+
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("Pause"):
@@ -20,19 +23,32 @@ func start_game() -> void:
 	add_child(new_game_manager)
 	
 	$TitleScreen.queue_free()
-	
+
+func dialogue_pause_switch() -> void:
+	in_dialogue_pause = not in_dialogue_pause
 	
 func pause_game() -> void:
-	if get_tree().paused:
+	
+	#logic when in dialogue pause
+	#if currently_paused:
+		#bring up the settings as normal. get_tree().paused will be true even though we still need to pause
+	
+	#basic logic when not in dialogue pause
+	if currently_paused:
 		#unpause the game
-		get_tree().paused = false
+		if not in_dialogue_pause: #in-combat dialogue is not up
+			get_tree().paused = false
 		#remove settings screen
 		$Settings.queue_free()
-	else:
+		
+		currently_paused = false
+	else: #not currently paused
 		#pause the game
 		get_tree().paused = true
 		#bring up settings screen
 		var new_settings = settings_scene.instantiate()
 		add_child(new_settings)
+		
+		currently_paused = true
 		
 		
