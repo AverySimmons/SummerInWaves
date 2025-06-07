b2a1ce772d789bbb3c23dcfbc15cbcb8ccf26914
Novali91 <joeyd@firetruk.ca>
2025-06-06 19:19:15 -0700

    Discs handle multiple collisions (with a for loop)

--- 02_Source/02_Combat/Discs/test_discs.gd
+++ 02_Source/02_Combat/Discs/test_discs.gd
@@ -9,10 +9,10 @@ func _ready() -> void:
 	Disc1.position.x += 200
 	Disc1.position.y += 200
 	Disc1.rotational_velocity = 0
-	Disc2.velocity = Vector2(0, 0)
+	Disc1.velocity = Vector2(0, 0)
+	Disc2.velocity = Vector2(-400, 0)
 	Disc2.position.x += 800
-	Disc2.position.y += 200
-	Disc2.velocity = Vector2(-6000, 0)
+	Disc2.position.y += 220
 	add_child(Disc1)
 	add_child(Disc2)
 	pass
