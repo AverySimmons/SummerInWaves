a742a164738973bfbbbf6b6fe83b38f893875d22
Novali91 <joeyd@firetruk.ca>
2025-06-07 13:05:53 -0700

    Fixed Disc inheritance to be goated!!!

--- 02_Source/02_Combat/Discs/SpecialDiscs/EnemyDiscs/enemy_disc.gd
+++ 02_Source/02_Combat/Discs/SpecialDiscs/EnemyDiscs/enemy_disc.gd
@@ -1,7 +1,7 @@
 extends "res://02_Source/02_Combat/Discs/disc.gd"
 
 var despawn_timer: float = 2
-var is_in_circle: bool = false	
+var is_in_ring: bool = false	
 
 func _physics_process(delta: float) -> void:
 	super._physics_process(delta)
@@ -9,4 +9,6 @@ func _physics_process(delta: float) -> void:
 	pass
 
 func despawn(delta: float) -> void:
-	
+	if $PlayspaceCheck.has_overlapping_areas():
+		pass
+	return
