439baaabcaa329f17cc61a082fb042a3bb64d6d2
Novali91 <joeyd@firetruk.ca>
2025-06-07 13:28:47 -0700

    Made max_velocity an export variable + ally_disc stuff

--- 02_Source/02_Combat/Discs/SpecialDiscs/player_disc.gd
+++ 02_Source/02_Combat/Discs/SpecialDiscs/player_disc.gd
@@ -5,12 +5,12 @@ var despawn_timer: float = 1
 func _physics_process(delta: float) -> void:
 	# Calls parent physics process
 	super._physics_process(delta)
-	despawn(delta)
+	despawn_check(delta)
 	pass
 
-func despawn(delta: float) -> void:
+func despawn_check(delta: float) -> void:
 	# If not moving, count down the despawn timer
-	if !is_moving():
+	if !super.is_moving():
 		despawn_timer -= delta
 		# Maybe could have an animation of slowly sinking/dissolving here?
 	else:
