7e2903d568e0514c0860481ab9512f5b4dbeb75c
Avery Simmons <eno116116@gmail.com>
2025-06-06 21:42:21 -0700

    okay maybe not

--- 02_Source/00_TopManagers/SettingsMenu/volume_slider.gd
+++ 02_Source/00_TopManagers/SettingsMenu/volume_slider.gd
@@ -6,20 +6,7 @@ var bus_index: int
 
 func _ready() -> void:
 	bus_index = AudioServer.get_bus_index(bus_name)
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 	value = AudioServer.get_bus_volume_db(bus_index)
 
 func _on_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(bus_index, value)
-=======
-=======
->>>>>>> Stashed changes
-	value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
-
-func _on_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
