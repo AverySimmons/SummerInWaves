15a9ca6b9ae26a418f0cce6e7a85d930f9464fa1
Sundered7 <brian@froods.ca>
2025-06-08 22:07:56 -0700

    Waves and combat music

--- 02_Source/00_TopManagers/TitleScreen/title_screen.gd
+++ 02_Source/00_TopManagers/TitleScreen/title_screen.gd
@@ -1,4 +1,9 @@
 extends Node2D
 
 func _on_texture_button_button_up() -> void:
+	$MenuClickLow.play()
 	SignalBus.start_game.emit()
+
+
+func _on_texture_button_mouse_entered() -> void:
+	$MenuClick.play()
